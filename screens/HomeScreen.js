fbd7419ccc098f6c66f5dadf11ce8e314bfa3ddb
usamasadiqus <usamasadiq029@gmail.com>
2023-01-07 16:10:01 +0500

    add expo app with tailwindcss and navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex items-center">
+      <Text className="text-red-500">App</Text>
+    </View>
+  );
+}
